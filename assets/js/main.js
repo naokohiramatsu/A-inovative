12740a6d82ae91cf8c322d25bc235cec09ce54d7
naokohiramatsu <naonomi1110@yahoo.co.jp>
2024-09-19 21:23:32 +0900

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+var hamburger = $('.hamburger__menu');
+
+
+$('.hamburger__button').on('click', function() {
+
+  hamburger.toggleClass('hamburger__menu-active');
+
+});
+
+
+$(window).on('resize', function() {
+
+  hamburger.removeClass('hamburger__menu-active');
+});
+
+
+$(function(){
+	$(window).scroll(function (){
+		$('.js-fade').each(function(){
+			var pos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > pos - windowHeight + 100){
+				$(this).addClass('scroll');
+			}
+		});
+	});
+});
+
